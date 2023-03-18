3b3e7add04d37e7912ddf1c7db890b05ef99b26f
kimbeomchang <fnzl89@gmail.com>
2023-03-18 15:40:42 +0900

    [feat] initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline text-center">beomlog</h1>
+			<p>App.tsx</p>
+		</div>
+	);
+};
+
+export default App;
