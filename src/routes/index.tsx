ebddc7dd04875234812ed6b0b37e129fc0201639
kimbeomchang <fnzl89@gmail.com>
2023-04-01 15:58:13 +0900

    [feat] 검색, 검색 결과 페이지 생성

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import { HomePage } from '@pages/Home';
+import { SearchPage } from '@pages/Search';
 
 export const Routes: React.FC = () => {
 	return (
@@ -11,6 +12,10 @@ export const Routes: React.FC = () => {
 					index: true,
 					path: '/',
 					element: <HomePage />,
+				},
+				{
+					path: 'search',
+					element: <SearchPage />,
 				},
 			])}
 		</div>
